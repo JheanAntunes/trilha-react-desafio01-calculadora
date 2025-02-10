f9f55e5e26a556d67d079d75b648722a86ba8b1c
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-10 11:03:06 -0300

    feat: Extract operation logic into custom hook for better separation of concerns

--- src/App.js
+++ src/App.js
@@ -2,61 +2,17 @@ import Input from "./components/Input";
 import Button from "./components/Button";
 
 import { Container, Content, Row } from "./styles";
-import { useState } from "react";
+import { useOperation } from "./hooks/operation";
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState("0");
-  const [firstNumber, setFirstNumber] = useState("0");
-  const [operation, setOperation] = useState("");
-
-  const handleOnClear = () => {
-    setCurrentNumber("0");
-    setFirstNumber("0");
-    setOperation("");
-  };
-
-  const handleAddNumber = (num) => {
-    setCurrentNumber((prev) => `${prev === "0" ? "" : prev}${num}`);
-  };
-
-  const handleSumNumbers = () => {
-    if (firstNumber === "0") {
-      setFirstNumber(String(currentNumber));
-      setCurrentNumber("0");
-      setOperation("+");
-    } else {
-      const sum = Number(firstNumber) + Number(currentNumber);
-      setCurrentNumber(String(sum));
-      setOperation("");
-    }
-  };
-
-  const handleMinusNumbers = () => {
-    if (firstNumber === "0") {
-      setFirstNumber(String(currentNumber));
-      setCurrentNumber("0");
-      setOperation("-");
-    } else {
-      const sum = Number(firstNumber) - Number(currentNumber);
-      setCurrentNumber(String(sum));
-      setOperation("");
-    }
-  };
-
-  const handleEquals = () => {
-    if (firstNumber !== "0" && operation !== "" && currentNumber !== "0") {
-      switch (operation) {
-        case "+":
-          handleSumNumbers();
-          break;
-        case "-":
-          handleMinusNumbers();
-          break;
-        default:
-          break;
-      }
-    }
-  };
+  const {
+    currentNumber,
+    handleOnClear,
+    handleAddNumber,
+    handleMinusNumbers,
+    handleSumNumbers,
+    handleEquals,
+  } = useOperation();
 
   return (
     <Container>
---
+++ src/hooks/operation.js
@@ -0,0 +1,65 @@
+import React from "react";
+export const useOperation = () => {
+  const [currentNumber, setCurrentNumber] = React.useState("0");
+  const [firstNumber, setFirstNumber] = React.useState("0");
+  const [operation, setOperation] = React.useState("");
+
+  const handleOnClear = () => {
+    setCurrentNumber("0");
+    setFirstNumber("0");
+    setOperation("");
+  };
+
+  const handleAddNumber = (num) => {
+    console.log("closures", num);
+    setCurrentNumber((prev) => `${prev === "0" ? "" : prev}${num}`);
+  };
+
+  const handleSumNumbers = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("+");
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation("");
+    }
+  };
+
+  const handleMinusNumbers = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("-");
+    } else {
+      const sum = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation("");
+    }
+  };
+
+  const handleEquals = () => {
+    if (firstNumber !== "0" && operation !== "" && currentNumber !== "0") {
+      switch (operation) {
+        case "+":
+          handleSumNumbers();
+          break;
+        case "-":
+          handleMinusNumbers();
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  return {
+    currentNumber,
+    handleOnClear,
+    handleAddNumber,
+    handleSumNumbers,
+    handleMinusNumbers,
+    handleEquals,
+  };
+};
